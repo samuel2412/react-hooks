98e3c09793138c8b56aee4f7a83be8909224b3ec
Samuel Alves <samuel241296@hotmail.com>
2020-01-03 14:17:10 -0300

    more useReducer

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState, useEffect, useCallback } from 'react';
+import React, { useReducer, useEffect, useCallback } from 'react';
 import axios from 'axios';
 
 import IngredientForm from './IngredientForm';
@@ -6,20 +6,30 @@ import IngredientList from './IngredientList';
 import ErrorModal from '../UI/ErrorModal'
 import Search from './Search';
 
-const ingredientReducer = (currentState,action) => {
-  switch(action.type){
+const ingredientReducer = (currentState, action) => {
+  switch (action.type) {
     case 'SET': return action.ingredients;
-    case 'ADD': return [...currentState,action.ingredient];
-    case 'DELETE': return currentState.filter(ing => ing.id !== action.id); 
+    case 'ADD': return [...currentState, action.ingredient];
+    case 'DELETE': return currentState.filter(ing => ing.id !== action.id);
     default: throw new Error('should not get there')
   }
 };
+const httpReducer = (currentState, action) => {
+  switch (action.type) {
+    case 'SEND': return { isLoading: true, error: null }
+    case 'RESPONSE': return { ...currentState, isLoading: false }
+    case 'ERROR': return { isLoading: false, error: action.messege }
+    case 'CLEAR': return {...currentState,error: null}
+    default: throw new Error('should not get there')
+  }
+}
 
 const Ingredients = () => {
   //const [ingredients, setIngredients] = useState([]);
-  const [ingredients,dispatch] = useReducer(ingredientReducer,[])
-  const [isLoading,setLoading] = useState(false);
-  const [error,setError] = useState(null);
+  //const [isLoading, setLoading] = useState(false);
+  //const [error, setError] = useState(null);
+  const [ingredients, dispatch] = useReducer(ingredientReducer, [])
+  const [httpState, httpDispatch] = useReducer(httpReducer,{isLoading:false,error:null})
 
   useEffect(() => {
     console.log("Rendering", ingredients)
@@ -27,14 +37,14 @@ const Ingredients = () => {
 
   const filterIngredientHandler = useCallback(ingredients => {
     //setIngredients(ingredients);
-    dispatch({type:'SET', ingredients})
+    dispatch({ type: 'SET', ingredients })
   }, []);
 
   const addIngredientHandler = ingredient => {
-    setLoading(true);
+    httpDispatch({type:'SEND'})
     axios.post('https://react-hooks-9208e.firebaseio.com/ingredients.json', ingredient)
       .then(response => {
-        setLoading(false);
+        httpDispatch({type:'RESPONSE'})
         /* setIngredients(prevIngredients => [
           ...prevIngredients,
           {
@@ -42,48 +52,45 @@ const Ingredients = () => {
             ...ingredient
           }
         ]) */
-        dispatch({type:'ADD', ingredient: {id: response.data.name, ...ingredient}})
+        dispatch({ type: 'ADD', ingredient: { id: response.data.name, ...ingredient } })
 
       })
       .catch(err => {
         console.log(err);
-        setError(`Something whent wrong!`);
-        setLoading(false);
+        httpDispatch({type:'ERROR',messege: 'Something whent wrong!'})
       });
   }
   const removeIngredientHandler = id => {
-    setLoading(true);
+    httpDispatch({type:'SEND'})
     axios.delete(`https://react-hooks-9208e.firebaseio.com/ingredients/${id}.json`)
       .then(response => {
-        console.log('removeIngredient')
-        setLoading(false);
+        httpDispatch({type:'RESPONSE'})
         /* setIngredients(
           ingredients.filter(ingredient => ingredient.id !== id)
         ) */
-        dispatch({type:'DELETE', id})
-      }).catch(err =>{
-        setError(`Something whent wrong!`);
-        setLoading(false);
+        dispatch({ type: 'DELETE', id })
+      }).catch(err => {
+        httpDispatch({type:'ERROR',messege: 'Something whent wrong!'})
       })
   }
 
-  const clearError = () =>{
-    setError(null);
+  const clearError = () => {
+    httpDispatch({type:'CLEAR'})
   };
 
   return (
     <div className="App">
 
-  {error === null ? null : <ErrorModal onClose={clearError} >{error}</ErrorModal>}
+      {httpState.error === null ? null : <ErrorModal onClose={clearError} >{httpState.error}</ErrorModal>}
 
-      <IngredientForm addIngredient={addIngredientHandler} isLoading={isLoading} />
+      <IngredientForm addIngredient={addIngredientHandler} isLoading={httpState.isLoading} />
 
       <section>
         <Search onFilter={filterIngredientHandler} />
         <IngredientList
 
-        ingredients={ingredients}
-        onRemoveItem={removeIngredientHandler} />
+          ingredients={ingredients}
+          onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
