cfee058f45268dc858af87e394c2bf3e008fd33e
Samuel Alves <samuel241296@hotmail.com>
2020-01-03 13:33:09 -0300

    loading error

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 import Card from '../UI/Card';
+import LoadingIndicator from '../UI/LoadingIndicator';
+
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
@@ -31,6 +33,7 @@ const IngredientForm = React.memo(props => {
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
+            {props.isLoading ? <LoadingIndicator/> : null}
           </div>
         </form>
       </Card>
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,10 +3,13 @@ import axios from 'axios';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal'
 import Search from './Search';
 
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
+  const [isLoading,setLoading] = useState(false);
+  const [error,setError] = useState(null);
 
   useEffect(() => {
     console.log("Rendering", ingredients)
@@ -17,8 +20,10 @@ const Ingredients = () => {
   }, []);
 
   const addIngredientHandler = ingredient => {
+    setLoading(true);
     axios.post('https://react-hooks-9208e.firebaseio.com/ingredients.json', ingredient)
       .then(response => {
+        setLoading(false);
         setIngredients(prevIngredients => [
           ...prevIngredients,
           {
@@ -30,26 +35,42 @@ const Ingredients = () => {
       })
       .catch(err => {
         console.log(err);
+        setError(`Something whent wrong!`);
+        setLoading(false);
       });
   }
   const removeIngredientHandler = id => {
+    setLoading(true);
     axios.delete(`https://react-hooks-9208e.firebaseio.com/ingredients/${id}.json`)
       .then(response => {
         console.log('removeIngredient')
+        setLoading(false);
         setIngredients(
           ingredients.filter(ingredient => ingredient.id !== id)
         )
+      }).catch(err =>{
+        setError(`Something whent wrong!`);
+        setLoading(false);
       })
-
   }
+
+  const clearError = () =>{
+    setError(null);
+  };
 
   return (
     <div className="App">
-      <IngredientForm addIngredient={addIngredientHandler} />
+
+  {error === null ? null : <ErrorModal onClose={clearError} >{error}</ErrorModal>}
+
+      <IngredientForm addIngredient={addIngredientHandler} isLoading={isLoading} />
 
       <section>
         <Search onFilter={filterIngredientHandler} />
-        <IngredientList ingredients={ingredients} onRemoveItem={removeIngredientHandler} />
+        <IngredientList
+
+        ingredients={ingredients}
+        onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
